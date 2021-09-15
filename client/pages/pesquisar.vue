f31b4d45ae1a7d198f1d7f572b0dc0a6c8d3bb92
Natanael dos Santos Feitosa <52074821+natanfeitosa@users.noreply.github.com>
2021-09-15 13:25:47 +0000

    Initial commit

--- client/pages/pesquisar.vue
+++ client/pages/pesquisar.vue
@@ -9,7 +9,7 @@
         @input="search"
       />
       <div class="w-full">
-        <button class="text-purple-500 background-transparent font-bold uppercase px-3 py-1 text-xs outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" @click="resetAll">
+        <button v-if="false" class="text-purple-500 background-transparent font-bold uppercase px-3 py-1 text-xs outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" @click="resetAll">
           <fa :icon="['fas', 'times']"/> Limpar filtros
         </button>
       </div>
